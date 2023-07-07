8be3a4163d137a47688f9a8251e061b5ee35d8f5
Adriana Sukhanova <adrianasuhanova@gmail.com>
2023-07-07 15:14:34 +0300

    Update ProfileInfo Comp Layout with Moddals

--- client/src/components/Home/components/ProfileInfo/ProfileInfo.jsx
+++ client/src/components/Home/components/ProfileInfo/ProfileInfo.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import Button from '../../../../common/Button/Button';
+import Modal from '../../../../common/Modal/Modal';
 import { logoutRequest } from '../../../../store/user/actionCreator';
 import { selectUserInfo } from '../../../../store/user/selectors';
 import { transformDate } from '../../../../helpers/transformDate';
+import {
+	selectModalError,
+	selectModalSuccess,
+} from '../../../../store/app/selectors';
 import {
 	BUTTON_TEXT_LOGOUT,
 	BUTTON_TEXT_CHANGE_PASS,
@@ -12,27 +17,48 @@ import {
 	BUTTON_TYPE_BUTTON,
 	USER_PHOTO_ALT_VALUE,
 	USER_PHOTO_SRC,
+	MODAL_TYPE_SUCCESS,
+	MODAL_TYPE_ERROR,
 } from '../../../../utils/constants';
 
 import './ProfileInfo.css';
+import ProfileModal from './components/ProfileModal';
 
 const ProfileInfo = () => {
+	const [modal, setModal] = useState(false);
+	const [changePassModal, setChangePassModal] = useState(false);
+	const [deleteAccModal, setDeleteAccModal] = useState(false);
+
 	const userInfo = useSelector(selectUserInfo);
+	const serverSuccess = useSelector(selectModalSuccess);
+	const serverError = useSelector(selectModalError);
 
 	const dispatch = useDispatch();
 
-	const handleChangePassword = () => {
-		console.log('password changed');
+	const changePassword = () => {
+		setModal(true);
+		setChangePassModal(true);
 	};
 
-	const handleDeleteAcc = () => {
-		console.log('Acc deleted');
+	const deleteAccount = () => {
+		setModal(true);
+		setDeleteAccModal(true);
+	};
+
+	const handleCloseModal = () => {
+		setModal(false);
+		setChangePassModal(false);
+		setDeleteAccModal(false);
 	};
 
 	const handleLogout = () => dispatch(logoutRequest());
 
 	return (
 		<div className='profile'>
+			{serverSuccess && (
+				<Modal type={MODAL_TYPE_SUCCESS} text={serverSuccess} />
+			)}
+			{serverError && <Modal type={MODAL_TYPE_ERROR} text={serverError} />}
 			<img
 				src={USER_PHOTO_SRC}
 				className='user-photo'
@@ -41,7 +67,7 @@ const ProfileInfo = () => {
 			/>
 			<div className='profile__content'>
 				<h3>{userInfo.name}</h3>
-				<p>{'asd12345@gmail.com'}</p>
+				<p>{userInfo.email}</p>
 				<h4 className='profile__role'>Role: {userInfo.role}</h4>
 				<p className='profile__date'>
 					Since {transformDate(userInfo.created_date)}
@@ -51,13 +77,13 @@ const ProfileInfo = () => {
 				className='profile__btn'
 				type={BUTTON_TYPE_BUTTON}
 				text={BUTTON_TEXT_CHANGE_PASS}
-				onClick={handleChangePassword}
+				onClick={changePassword}
 			/>
 			<Button
 				className='profile__btn profile__btn-delete'
 				type={BUTTON_TYPE_BUTTON}
 				text={BUTTON_TEXT_DELETE_ACC}
-				onClick={handleDeleteAcc}
+				onClick={deleteAccount}
 			/>
 			<Button
 				className='profile__btn profile__btn-logout'
@@ -65,6 +91,13 @@ const ProfileInfo = () => {
 				text={BUTTON_TEXT_LOGOUT}
 				onClick={handleLogout}
 			/>
+			{modal && (
+				<ProfileModal
+					changePassModal={changePassModal}
+					deleteAccModal={deleteAccModal}
+					handleCloseModal={handleCloseModal}
+				/>
+			)}
 		</div>
 	);
 };
